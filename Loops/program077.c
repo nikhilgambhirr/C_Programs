b1660a9c00af5d9255eb0d99e5e6d868f70a36ea
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:07:34 +0530

    Add files via upload

---
+++ Loops/program077.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+     char ch = '\0';            // ha fackt Division slove karnar ha program
+
+
+     printf("Enetr your division : \n");
+     scanf("%c",&ch);
+
+     printf("your Division is : %c\n",ch);
+
+
+    return 0;
+}
---
+++ Loops/program078.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+                            // ASCII value show karto ha program 
+int main()
+{
+     char ch = '\0';
+
+
+     printf("Enetr character : \n");
+     scanf("%c",&ch);
+
+     printf("ASCII value of %c is %d\n",ch,ch);
+
+
+    return 0;
+}
